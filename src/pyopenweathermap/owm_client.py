aa6c3b47a3b94c031d20b7b394954b705f0ef3a3
Evgeny <iam@freekode.org>
2024-05-01 19:55:43 +0200

    support onecall api v25

--- src/pyopenweathermap/owm_client.py
+++ src/pyopenweathermap/owm_client.py
@@ -3,6 +3,7 @@ from aiohttp import ClientSession
 from .data_converter import DataConverter
 from .exception import UnauthorizedError, RequestError, TooManyRequestsError
 from .weather import WeatherReport
+import logging
 
 API_V30_URL = 'https://api.openweathermap.org/data/3.0/onecall'
 API_V25_URL = 'https://api.openweathermap.org/data/2.5/onecall'
@@ -12,8 +13,10 @@ WEATHER_TYPES = {'current', 'minutely', 'hourly', 'daily', 'alerts'}
 class OWMClient:
     session: ClientSession | None = None
     request_timeout: int
+    logger = logging.getLogger(__name__)
 
     def __init__(self, api_key, api_version, units="metric", lang='en', request_timeout=20):
+        self.logger.info('Initializing OWMClient with api version: ' + str(api_version))
         if api_version == 'v3.0':
             self.main_url = API_V30_URL
         elif api_version == 'v2.5':
@@ -45,12 +48,14 @@ class OWMClient:
     async def validate_key(self) -> bool:
         url = self._get_url(50.06, 14.44, WEATHER_TYPES)
         try:
+
             await self._request(url)
             return True
         except UnauthorizedError:
             return False
 
     async def _request(self, url):
+        self.logger.debug('Requesting url: ' + url)
         async with ClientSession() as session:
             try:
                 async with session.get(url=url, timeout=self.request_timeout) as response:
@@ -67,10 +72,10 @@ class OWMClient:
                         raise TooManyRequestsError(response_json.get('message'))
                     else:
                         raise RequestError("Unknown status code: {}".format(response.status))
-            except TimeoutError:
-                raise RequestError("Request timeout")
             except RequestError as error:
                 raise error
+            except TimeoutError:
+                raise RequestError("Request timeout")
             except Exception as error:
                 raise RequestError(error) from error
 
