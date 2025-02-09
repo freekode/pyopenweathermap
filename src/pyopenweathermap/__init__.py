5b0e4faa4556a94e83c674dc7f58141f103f246e
wittypluck <wittypluck@users.noreply.github.com>
2025-02-09 18:42:12 +0100

    Expose CurrentAirPollution and AirPollutionReport

--- src/pyopenweathermap/__init__.py
+++ src/pyopenweathermap/__init__.py
@@ -8,3 +8,4 @@ from .client.owm_client_factory import create_owm_client
 from .weather import (
     CurrentWeather, MinutelyWeatherForecast, HourlyWeatherForecast, DailyWeatherForecast, WeatherReport, DailyTemperature, WeatherCondition
 )
+from .air_pollution import CurrentAirPollution, AirPollutionReport
