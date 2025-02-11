c6c814cf89065bce16b4a7921b36e6c5a0cb04b0
Evgeny <iam@freekode.org>
2025-02-11 18:54:51 +0100

    refactot air pollution

--- src/pyopenweathermap/__init__.py
+++ src/pyopenweathermap/__init__.py
@@ -4,9 +4,9 @@ from .exception import (
     TooManyRequestsError
 )
 from .client.weather.abstract_weather_client import OWMWeatherClient
-from .client.air_pollution_client import AirPollutionReport
 from .client.owm_client_factory import create_owm_client
 from .weather import (
     CurrentWeather, MinutelyWeatherForecast, HourlyWeatherForecast, DailyWeatherForecast, WeatherReport, DailyTemperature, WeatherCondition
 )
+from .client.air_pollution_client import AirPollutionReport
 from .air_pollution import CurrentAirPollution, AirPollutionReport
